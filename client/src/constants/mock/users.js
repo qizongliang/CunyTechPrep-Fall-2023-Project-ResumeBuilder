ed2cb2ba0bb113f8865184374888f935e98c922a
butterriolu <derpvin12@gmail.com>
2022-10-28 21:43:32 -0400

    added mock data

---
+++ client/src/constants/mock/users.js
@@ -0,0 +1,19 @@
+export const MOCK_USERS = [
+  {
+    profile_picture_url:
+      "https://storage.googleapis.com/katsudon-assets/user-profiles/Devin.jpg",
+    username: "Butterriolu",
+    description: "Undergraduate Teaching Assistant at CUNY Hunter College",
+  },
+  {
+    profile_picture_url: null,
+    username: "FBI Agent",
+    description: "Software Engineer at Burger King",
+  },
+  {
+    profile_picture_url:
+      "https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec",
+    username: "Amongus",
+    description: null,
+  },
+];
