1b76c6f35c83eb868b04505d7636ab38759e66ad
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-11-06 21:48:16 -0500

    resolve merge conflict

--- client/src/pages/SearchUserPage.js
+++ client/src/pages/SearchUserPage.js
@@ -2,30 +2,31 @@ import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 import ErrorAlert from "../components/ErrorAlert";
 import {MOCK_USERS} from "../constants/mock/users";
+import Avatar from '@mui/material/Avatar';
 
 function SearchUserPage() {
-  let userView = () =>{
-    return MOCK_USERS.map((user)=>{
-      
-      return (
-        <>
-          <div class="container border border-secondary rounded pull-left">
-            <div class="row">
-              <div class="col-1 m-0 p-0">
-                <img src={user.profile_picture_url} alt="pic" class="img-fluid rounded-circle start m-auto p-0 float-left" width="32px"/>
-              </div>
-              <div class="col-3 m-auto p-0">
-                <p class="text-start m-0 Container-fluid p-0" >{user.username} </p>
-              </div>
-              <div class="col-8 m-auto p-0">
-                <p class="text-end m-0 Container-fluid p-0">{user.description}</p>
-              </div>
+
+  let  userView = MOCK_USERS.map((user)=>{
+    return (
+        <div class="container border border-secondary rounded pull-left" key={user.id}>
+          <div class="row">
+            <div class="col-1 m-0 p-0">
+              <img src={user.profile_picture_url?? 
+              "https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec"} 
+              alt="" class="img-fluid rounded-circle start m-auto p-0 float-left" 
+              width="32px" height="32px"/>
+            </div>
+            <div class="col-3 m-auto p-0">
+              <p class="text-start m-0 Container-fluid p-0" >{user.username} </p>
+            </div>
+            <div class="col-8 m-auto p-0">
+              <p class="text-end m-0 Container-fluid p-0">{user.description}</p>
             </div>
           </div>
-        </>
-      )
-    })
-  }
+        </div>
+    )
+  });
+  
   const [content, setContent] = useState("");
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
@@ -59,7 +60,7 @@ function SearchUserPage() {
           </button>
         </div>
       </form>
-      {userView()}
+      {userView}
     </div>
   );
 }
