a63ed782d234e4428f85518fbbe668f9633103e3
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-11-17 04:04:24 -0500

    portfolio page aboutme, work Experience, project and navbar

--- client/src/pages/UserProfilePage.js
+++ client/src/pages/UserProfilePage.js
@@ -4,6 +4,7 @@ import ErrorAlert from '../components/ErrorAlert'
 import { MOCK_USERS } from '../constants/mock/users'
 import Avatar from '@mui/material/Avatar'
 import { MOCK_PROFILES } from '../constants/mock/profile'
+import './UserProfilePage.css'
 
 function UserProfilePage() {
   // const [allUsers, setAllUsers] = useState([])
@@ -25,24 +26,88 @@ function UserProfilePage() {
 
   const profile_picture_placeholder =
     'https://play-lh.googleusercontent.com/8ddL1kuoNUB5vUvgDVjYY3_6HwQcrg1K2fd_R8soD-e2QYj8fT9cfhfh3G0hnSruLKec'
-  let userView = function (user, profile) {
+
+  // Renders Banner
+  let banner = (user) => {
     return (
-      <div className="container-fluid bg-dark">
-        <img
-          src={user[0].profile_picture_url ?? profile_picture_placeholder}
-          alt=""
-          class="img-fluid rounded-circle start m-auto p-0 float-left"
-          width="150px"
-          height="150px"
-        />
-        <div className="container-fluid bg-dark">
-          <div></div>
-          <p className="text-center m-0 Container-fluid p-0 text-white">
+      <>
+        <div className="container-fluid banner_bg">
+          <img
+            src={user[0].profile_picture_url ?? profile_picture_placeholder}
+            alt=""
+            class="img-fluid rounded-circle start m-auto float-left"
+            width="150px"
+            height="150px"
+          />
+        </div>
+        <div className="container-fluid banner_bg">
+          <p className="text-center Container-fluid m-0 p-0 text-white">
             {user[0].username}{' '}
           </p>
-          <div></div>
         </div>
-      </div>
+      </>
+    )
+  }
+  //render Resume Navbar
+  let resumeNavBar = () => {
+    return (
+      <>
+        <div className="tab">
+          <button className="tablinks">Details</button>
+          <button className="tablinks">Portfolio</button>
+          <button className="tablinks">Resume</button>
+        </div>
+      </>
+    )
+  }
+  //Renders About Me
+  let aboutMe = () => {
+    return (
+      <>
+        <div className="container_a">
+          <div className="lines_a">
+            <div className="sectionTitle_a">About Me</div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  //Renders workExperience
+  let workExperience = () => {
+    return (
+      <>
+        <div className="container_w">
+          <div className="lines_w">
+            <div className="sectionTitle_w">Work Experience</div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  //Renders Project
+  let project = () => {
+    return (
+      <>
+        <div className="container_p">
+          <div className="lines_p">
+            <div className="sectionTitle_p">Project</div>
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  let userView = function (user, profile) {
+    return (
+      <>
+        {banner(user)}
+        <div className="resume_bg">
+          {resumeNavBar()}
+          {aboutMe()}
+          {workExperience()}
+          {project()}
+        </div>
+      </>
     )
   }
 
