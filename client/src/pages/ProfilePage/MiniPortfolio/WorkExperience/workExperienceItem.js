bf830e94cd23f44faca6fe0f13e66595431754a2
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-11-28 00:12:34 -0500

    change workExperience Item format for display

--- client/src/pages/ProfilePage/MiniPortfolio/WorkExperience/workExperienceItem.js
+++ client/src/pages/ProfilePage/MiniPortfolio/WorkExperience/workExperienceItem.js
@@ -4,7 +4,7 @@ import { Card, CardContent, Typography, Grid } from "@mui/material";
 let displayEnddate = (end_month, end_year) => {
   return (
     <>
-      {end_month}
+      {displayMonthInWord(end_month)}
       {end_year}
     </>
   );
