24e99e0587243a633e14b8e7e004b28bed0607ae
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-11-27 23:34:29 -0500

    delete some unused code and added new files

--- client/src/pages/ProfilePage/MiniPortfolio/WorkExperience/workExperienceItem.js
+++ client/src/pages/ProfilePage/MiniPortfolio/WorkExperience/workExperienceItem.js
@@ -1,7 +1,5 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import { Card, CardContent, Typography, Grid } from "@mui/material";
-import RiceBowlIcon from "@mui/icons-material/RiceBowl";
 
 let displayEnddate = (end_month, end_year) => {
   return (
