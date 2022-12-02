7fa954693c677e7e6e8eb7d36f98a766a673ed20
qizongliang <72285030+qizongliang@users.noreply.github.com>
2022-12-02 17:17:02 -0500

    delete unneccessary stuff

--- client/src/pages/ProfilePage/MiniPortfolio/index.js
+++ client/src/pages/ProfilePage/MiniPortfolio/index.js
@@ -6,7 +6,6 @@ export default function MiniPortfolio(props) {
   return (
     <>
       <AboutMe userInfo={props.userInfo} />
-      <br />
       <WorkExperience workItems={props.workExperiences} />
       <Project projects={props.projects} />
       <br />
